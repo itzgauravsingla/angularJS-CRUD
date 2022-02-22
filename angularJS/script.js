8465de6891efe1d5a35f3aa4b3e44a2408afd683
Gaurav Singla <97586538+itzgauravsingla@users.noreply.github.com>
2022-02-22 11:32:54 +0530

    Used constant method

--- angularJS/script.js
+++ angularJS/script.js
@@ -1,59 +1,57 @@
 var myApp = angular.module("myApp", []);
-myApp.controller("fetchAPI", ["$scope", "$http", ($scope, $http) => {
-    // $scope.inform = [];
-    const readAPI = 'https://3gu8fobdu7.execute-api.us-east-2.amazonaws.com/v1/api/msbdocs/getmessage?operation=read&operation=read';
-    $http({method : 'GET',url : readAPI})
-    .then(function(data, status) {
+myApp.controller("fetchAPI", [
+  "$scope",
+  "$http",
+  "ApiBase",
+  ($scope, $http, ApiBase) => {
+    const readAPI =
+      (ApiBase + "?operation=read&operation=read");
+    $http({ method: "GET", url: readAPI })
+      .then(function (data, status) {
         console.log(data.data.Items);
         $scope.apiData = data.data.Items;
-        // console.log(apiData.id)
-        // console.log(apiData.description)
-        // $scope.inform.push([apiData.id, apiData.description]);
-    })
-    .catch(function(data, status) {
+      })
+      .catch(function (data, status) {
         alert("Error");
-    });
+      });
 
-    $scope.editAPI = function()
-    {
-        // console.log(key);
-        let id = $scope.inID;
-        let des = $scope.inDES; 
-        const putAPI = `https://3gu8fobdu7.execute-api.us-east-2.amazonaws.com/v1/api/msbdocs/getmessage?operation=update&id=${id}&description=${des}`;
-        $http({method: 'PUT', url : putAPI})
-        .then(function(data){
-            alert("updated");
-        }).catch( function(data,status){
-            alert("Error " + status)
+    $scope.editAPI = function () {
+      // console.log(key);
+      let id = $scope.inID;
+      let des = $scope.inDES;
+      const putAPI = (ApiBase + `?operation=update&id=${id}&description=${des}`);
+      $http({ method: "PUT", url: putAPI })
+        .then(function (data) {
+          alert("updated");
+        })
+        .catch(function (data, status) {
+          alert("Error " + status);
         });
-    }
+    };
 
-    $scope.deleteAPI = function(id)
-    {
-        const delAPI = `https://3gu8fobdu7.execute-api.us-east-2.amazonaws.com/v1/api/msbdocs/getmessage?&operation=delete&id=${id}`;
-        $http({method: "DELETE", url: delAPI})
-        .then(()=> alert("deleted"))
-        .catch(()=>alert("error"));
-    }
+    $scope.deleteAPI = function (id) {
+      const delAPI = (ApiBase + `?&operation=delete&id=${id}`);
+      $http({ method: "DELETE", url: delAPI })
+        .then(() => alert("deleted"))
+        .catch(() => alert("error"));
+    };
 
-    $scope.addAPI = function()
-    {
-        let id = $scope.inID;
-        let des = $scope.inDES;
-        const postAPI = `https://3gu8fobdu7.execute-api.us-east-2.amazonaws.com/v1/api/msbdocs/getmessage`;
-        addData = {
-            operation: 'create',
-            payload:{
-                Item: {
-                    "description": $scope.inDES,
-                    "id": $scope.inID
-                }
-            }
-            
-        }
-        $http({method: "POST", url: postAPI, data: addData })
-        .then(()=> alert("Posted"))
-        .catch(()=>alert("error"));
-    }
-
-}]);
+    $scope.addAPI = function () {
+      let id = $scope.inID;
+      let des = $scope.inDES;
+      const postAPI = ApiBase;
+      addData = {
+        operation: "create",
+        payload: {
+          Item: {
+            description: $scope.inDES,
+            id: $scope.inID,
+          },
+        },
+      };
+      $http({ method: "POST", url: postAPI, data: addData })
+        .then(() => alert("Posted"))
+        .catch(() => alert("error"));
+    };
+  },
+]).constant("ApiBase", "https://3gu8fobdu7.execute-api.us-east-2.amazonaws.com/v1/api/msbdocs/getmessage");
