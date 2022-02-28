a9f86c8bf646ff7e6d5ae479cfd7082f15a3b270
Gaurav Singla <97586538+itzgauravsingla@users.noreply.github.com>
2022-02-28 13:04:46 +0530

    Updates the API data view without need of refresh

--- angularJS/script.js
+++ angularJS/script.js
@@ -1,57 +1,74 @@
 var myApp = angular.module("myApp", []);
-myApp.controller("fetchAPI", [
-  "$scope",
-  "$http",
-  "ApiBase",
-  ($scope, $http, ApiBase) => {
-    const readAPI =
-      (ApiBase + "?operation=read&operation=read");
-    $http({ method: "GET", url: readAPI })
-      .then(function (data, status) {
-        console.log(data.data.Items);
-        $scope.apiData = data.data.Items;
-      })
-      .catch(function (data, status) {
-        alert("Error");
-      });
-
-    $scope.editAPI = function () {
-      // console.log(key);
-      let id = $scope.inID;
-      let des = $scope.inDES;
-      const putAPI = (ApiBase + `?operation=update&id=${id}&description=${des}`);
-      $http({ method: "PUT", url: putAPI })
-        .then(function (data) {
-          alert("updated");
+myApp
+  .controller("fetchAPI", [
+    "$scope",
+    "$http",
+    "ApiBase",
+    ($scope, $http, ApiBase) => {
+      // $scope.inform = [];
+      
+      $scope.getAPI = function() {
+        const readAPI = ApiBase + "?operation=read&operation=read";
+      $http({ method: "GET", url: readAPI })
+        .then(function (data, status) {
+          console.log(data.data.Items);
+          $scope.apiData = data.data.Items;
+          // console.log(apiData.id)
+          // console.log(apiData.description)
+          // $scope.inform.push([apiData.id, apiData.description]);
         })
         .catch(function (data, status) {
-          alert("Error " + status);
+          alert("Error");
         });
-    };
-
-    $scope.deleteAPI = function (id) {
-      const delAPI = (ApiBase + `?&operation=delete&id=${id}`);
-      $http({ method: "DELETE", url: delAPI })
-        .then(() => alert("deleted"))
-        .catch(() => alert("error"));
-    };
-
-    $scope.addAPI = function () {
-      let id = $scope.inID;
-      let des = $scope.inDES;
-      const postAPI = ApiBase;
-      addData = {
-        operation: "create",
-        payload: {
-          Item: {
-            description: $scope.inDES,
-            id: $scope.inID,
-          },
-        },
       };
-      $http({ method: "POST", url: postAPI, data: addData })
-        .then(() => alert("Posted"))
-        .catch(() => alert("error"));
-    };
-  },
-]).constant("ApiBase", "https://3gu8fobdu7.execute-api.us-east-2.amazonaws.com/v1/api/msbdocs/getmessage");
+
+      $scope.getAPI();
+
+      $scope.editAPI = function () {
+        // console.log(key);
+        let id = $scope.inID;
+        let des = $scope.inDES;
+        const putAPI =
+          ApiBase + `?operation=update&id=${id}&description=${des}`;
+        $http({ method: "PUT", url: putAPI })
+          .then(function (data) {
+            alert("updated");
+            $scope.getAPI();
+          })
+          .catch(function (data, status) {
+            alert("Error " + status);
+          });
+      };
+
+      $scope.deleteAPI = function (id) {
+        const delAPI = ApiBase + `?&operation=delete&id=${id}`;
+        $http({ method: "DELETE", url: delAPI })
+          .then(() => alert("deleted"))
+          .then(() => { $scope.getAPI(); })
+          .catch(() => alert("error"));
+      };
+
+      $scope.addAPI = function () {
+        let id = $scope.inID;
+        let des = $scope.inDES;
+        const postAPI = ApiBase;
+        addData = {
+          operation: "create",
+          payload: {
+            Item: {
+              description: $scope.inDES,
+              id: $scope.inID,
+            },
+          },
+        };
+        $http({ method: "POST", url: postAPI, data: addData })
+          .then(() => alert("Posted"))
+          .then(() => { $scope.getAPI(); })
+          .catch(() => alert("error"));
+      };
+    },
+  ])
+  .constant(
+    "ApiBase",
+    "https://3gu8fobdu7.execute-api.us-east-2.amazonaws.com/v1/api/msbdocs/getmessage"
+  );
